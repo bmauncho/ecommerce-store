c95ff5d1018b5c0d47a053b3134a346ebb86d973
BradleyMauncho <bradley@mekangames.com>
2026-06-17 00:20:34 +0300

    update

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Hello store</h1>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ app/components/main-nav.tsx
@@ -0,0 +1,5 @@
+const MainNav = () => {
+  return <nav>MainNav</nav>;
+};
+
+export default MainNav;
